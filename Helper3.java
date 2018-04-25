da6acbcd55621c0781649647cc5ea9fdb7e8f605
Carmerada <31554846+Carmerada@users.noreply.github.com>
2018-04-24 19:40:18 -1000

    Commit

---
+++ Helper3.java
@@ -0,0 +1,15 @@
+ package helper3;
+import java.util.Scanner;
+/** ***********************
+* @author LUNDY_BURGE
+* @assignment CSCI 2912
+* @Date: _put_date_here_
+* Assignment: _put_name_of_assignment_here_ *
+* Algorithm *
+*
+* ***********************/
+public class Helper3 {
+public static void main(String[] args) {
+    System.out.println("Good diet is important.");
+} // end of main()
+} // end of Helper3 class
