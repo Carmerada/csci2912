8dddbf78cb9089e5ab72be4dc0d85a4f677a1cc8
Carmerada <31554846+Carmerada@users.noreply.github.com>
2018-04-24 18:27:19 -1000

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+ package hello;
+import java.util.Scanner;
+/** ***********************
+* @author LUNDY_BURGE
+* @assignment CSCI 2912
+* @Date: _put_date_here_
+* Assignment: _put_name_of_assignment_here_ *
+* Algorithm *
+*
+* ***********************/
+public class Hello {
+public static void main(String[] args) {
+    System.out.println("hello");
+} // end of main()
+} // end of Hello class
