9d2f03c68f1b65598dc438530bcc6ef424efd75e
Carmerada <31554846+Carmerada@users.noreply.github.com>
2018-04-24 18:34:26 -1000

    Initial Commit

---
+++ Helper.java
@@ -0,0 +1,15 @@
+ package helper;
+import java.util.Scanner;
+/** ***********************
+* @author LUNDY_BURGE
+* @assignment CSCI 2912
+* @Date: _put_date_here_
+* Assignment: _put_name_of_assignment_here_ *
+* Algorithm *
+*
+* ***********************/
+public class Helper {
+public static void main(String[] args) {
+    System.out.println("Staring right into the sun is not a fun time.");
+} // end of main()
+} // end of Helper class
